8b8301e06e0ad3dd2a643a042a384020ae0e20c1
marcozzxx810 <marcowingwing@gmail.com>
2022-02-16 21:22:41 +0800

    feat: image to ascii

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request, url_for
+from werkzeug.utils import secure_filename
+from asciitool import imageToAscii
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = os.path.dirname(__file__) + '/static/images'
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def findFileExtension(filename):
+    return filename.rsplit('.', 1)[1].lower()
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route('/convert', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+        if 'image' not in request.files:
+            return 'Failed'
+        file = request.files['image']
+        if file.filename == '':
+            return 'Failed'
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            fileExtension = findFileExtension(filename)
+            filePath = os.path.join(app.config['UPLOAD_FOLDER'], "input."+fileExtension)
+            outputPath = os.path.join(app.config['UPLOAD_FOLDER'], "out.txt")
+            file.save(filePath)
+            imageToAscii(filePath, outputPath)
+        return redirect(url_for('result'))
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    if request.method == 'GET':
+        return render_template('result.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ asciitool.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import numpy as np
+
+gscale1 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+
+def map_matrix_gscale(normalised_array):
+    height, width = normalised_array.shape
+    string_array = np.empty((height, width)).astype('str')
+    for i in range(height):
+        for j in range(width):
+            string_array[i][j] = gscale1[normalised_array[i][j]]
+    return string_array
+
+
+def scale_matrix(image_array):
+    return (image_array*(len(gscale1)-1)).astype('int')
+
+def average_image(image: Image, width: int, height: int):
+    average_array = np.empty((height, width))
+    image_array = np.array(image)
+    for i in range(height):
+        for j in range(width):
+            average_array[i][j] = np.mean(image_array[i][j])/255
+    return average_array
+
+def writeStringToFile(string_matrix, outputFilePath):
+    file = open(outputFilePath, "w")
+    for row in string_matrix:
+        file.write(''.join(row)+'\n')
+def imageToAscii(filePath, outputFilePath):
+    im = Image.open(filePath)
+    rgb_im = im.convert('RGB')
+    width, height = rgb_im.size
+    average_im = average_image(rgb_im, int(width), int(height))
+    normalized_matrix = scale_matrix(average_im)
+    string_matrix = map_matrix_gscale(normalized_matrix)
+
+    writeStringToFile(string_matrix, outputFilePath)
+
